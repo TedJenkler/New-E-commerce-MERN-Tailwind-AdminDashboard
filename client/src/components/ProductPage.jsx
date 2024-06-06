bc2a8d2d91ebe858bbeafa391f90d1db97a64215
Ted Jenkler <tedjenkler@gmail.com>
2024-06-06 06:21:07 +0200

    product info styled

--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../features/shopSlice';
 import { useParams } from 'react-router';
+import listen from '../assets/listen.png'
 
 function productPage() {
     const dispatch = useDispatch()
@@ -15,45 +16,51 @@ function productPage() {
         dispatch(fetchData());
     }, []);
 
+    const splitByTag = (str) => {
+      return str.split("<br>")
+    }
+
   return (
-    <div>
-      <div>
-        <p>Go Back</p>
+    <div className='px-6'>
+      <div className='py-4'>
+        <p className='text-sbase font-medium text-bordergrey'>Go Back</p>
       </div>
       <section>
-        <img src={product ? product.img : null} alt="product img"/>
-        <h1>{product ? product.name : null}</h1>
-        <p>{product ? product.description : null}</p>
-        <p>$ {product ? product.price : null}</p>
+        <img className='mb-8' src={product ? product.img : null} alt="product img"/>
+        <h1 className='text-28xl font-bold text-black2 mb-6'>{product ? product.name : null}</h1>
+        <p className='text-sbase text-bordergrey font-medium mb-6'>{product ? product.description : null}</p>
+        <p className='text-lg text-black2 font-bold mb-8'>$ {product ? product.price : null}</p>
         <div>
 
         </div>
-        <h2>FEATURES</h2>
-        <p>{product ? product.features : null}</p>
-        <h2>IN THE BOX</h2>
+        <h2 className='text-2xl text-black2 font-bold mb-6'>FEATURES</h2>
+        <p className='text-sbase text-bordergrey font-medium mb-6'>{product ? splitByTag(product.features)[0] : null}</p>
+        <p className='text-sbase text-bordergrey font-medium mb-28'>{product ? splitByTag(product.features)[1] : null}</p>
+        <h2 className='text-2xl text-black2 font-bold mb-6'>IN THE BOX</h2>
         {product ? product.includes.map((item) => {
           return ( 
-          <>
-            <p>{item.quantity}</p>
-            <p>{item.item}</p>
-          </>
+          <div className='flex gap-5 mb-2'>
+            <p className='text-sbase text-darkorange font-medium'>{item.quantity}x</p>
+            <p className='text-sbase text-bordergrey font-medium'>{item.item}</p>
+          </div>
           )
         }) : null}
-        <img src={product ? product.gallery.first.mobile : null} alt="gallery1" />
-        <img src={product ? product.gallery.second.mobile : null} alt="gallery2" />
-        <img src={product ? product.gallery.third.mobile : null} alt="gallery2" />
-        <h3>YOU MAY ALSO LIKE</h3>
+        <img className='mt-24 mb-4' src={product ? product.gallery.first.mobile : null} alt="gallery1" />
+        <img className='mb-4' src={product ? product.gallery.second.mobile : null} alt="gallery2" />
+        <img className='mb-32' src={product ? product.gallery.third.mobile : null} alt="gallery2" />
+        <h3 className='text-2xl text-black2 font-bold text-center mb-10'>YOU MAY ALSO LIKE</h3>
         {product ? product.others.map((item) => {
           return (
-            <>
-              <img src={item.image.mobile} />
-              <p>{item.name}</p>
-            </>
+            <div className='flex flex-col items-center mb-14'>
+              <img className='mb-8' src={item.image.mobile} />
+              <p className='text-2xl font-bold text-black2 text-center mb-8'>{item.name}</p>
+              <button className='bg-darkorange text-white px-8 py-4'>SEE PRODUCT</button>
+            </div>
           )
         }) : null}
-        <img />
-        <h3>BRINGING YOU THE <span>BEST</span> AUDIO GEAR</h3>
-        <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+        <img className='mb-10' src={listen} alt='listen' />
+        <h3 className='text-28xl text-black2 font-bold text-center mb-8'>BRINGING YOU THE <span className='text-28xl text-darkorange font-bold'>BEST</span> AUDIO GEAR</h3>
+        <p className='text-sbase text-bordergrey font-medium mb-32'>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
       </section>
     </div>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'sbase': '0.938rem',
+        '28xl': '1.75rem',
+      },
+      colors: {
+        'darkorange': '#D87D4A',
+        'lightorange': '#fbaf85',
+        'black1': '#101010',
+        'white': '#FFFFFF',
+        'greywhite': '#F1F1F1',
+        'black2': '#000000',
+        'lightgreywhite': '#FAFAFA',
+        'bordergrey': '#979797',
+        'hoverblack': '#4C4C4C',
+      }
+    },
   },
   plugins: [],
 }
