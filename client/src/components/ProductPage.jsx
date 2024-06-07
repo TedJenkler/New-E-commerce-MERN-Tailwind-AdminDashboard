ddd3b92f161c1e65367941dab1006f6ca9062089
Ted Jenkler <tedjenkler@gmail.com>
2024-06-07 07:22:31 +0200

    added new product text

--- client/src/components/ProductPage.jsx
+++ client/src/components/ProductPage.jsx
@@ -27,9 +27,10 @@ function productPage() {
       </div>
       <section>
         <img className='mb-8' src={product ? product.img : null} alt="product img"/>
-        <h1 className='text-28xl font-bold text-black2 mb-6'>{product ? product.name : null}</h1>
+        <p className='text-darkorange text-sbase tracking-[10px] mb-6'>{product ? product.newP ? "NEW PRODUCT" : null : null}</p>
+        <h1 className='text-28xl font-bold text-black2 mb-6 tracking-[1px]'>{product ? product.name : null}</h1>
         <p className='text-sbase text-bordergrey font-medium mb-6'>{product ? product.description : null}</p>
-        <p className='text-lg text-black2 font-bold mb-8'>$ {product ? product.price : null}</p>
+        <p className='text-lg text-black2 font-bold mb-8 tracking-[1.29px]'>$ {product ? product.price : null}</p>
         <div className='flex mb-24'>
           <div className='flex w-1/2'>
             <div className='flex w-[120px] h-[48px] bg-greywhite'>
@@ -42,10 +43,10 @@ function productPage() {
             <button className='bg-darkorange text-white px-8 py-4 text-lxs'>ADD TO CART</button>
           </div>
         </div>
-        <h2 className='text-2xl text-black2 font-bold mb-6'>FEATURES</h2>
+        <h2 className='text-2xl text-black2 font-bold mb-6 tracking-[0.86px]'>FEATURES</h2>
         <p className='text-sbase text-bordergrey font-medium mb-6'>{product ? splitByTag(product.features)[0] : null}</p>
         <p className='text-sbase text-bordergrey font-medium mb-28'>{product ? splitByTag(product.features)[1] : null}</p>
-        <h2 className='text-2xl text-black2 font-bold mb-6'>IN THE BOX</h2>
+        <h2 className='text-2xl text-black2 font-bold mb-6 tracking-[0.86px]'>IN THE BOX</h2>
         {product ? product.includes.map((item) => {
           return ( 
           <div className='flex gap-5 mb-2'>
@@ -57,7 +58,7 @@ function productPage() {
         <img className='mt-24 mb-4' src={product ? product.gallery.first.mobile : null} alt="gallery1" />
         <img className='mb-4' src={product ? product.gallery.second.mobile : null} alt="gallery2" />
         <img className='mb-32' src={product ? product.gallery.third.mobile : null} alt="gallery2" />
-        <h3 className='text-2xl text-black2 font-bold text-center mb-10'>YOU MAY ALSO LIKE</h3>
+        <h3 className='text-2xl text-black2 font-bold text-center mb-10 tracking-[0.86px]'>YOU MAY ALSO LIKE</h3>
         {product ? product.others.map((item) => {
           return (
             <div className='flex flex-col items-center mb-32'>
@@ -69,7 +70,7 @@ function productPage() {
         }) : null}
         <CategoryLinks />
         <img className='mb-10' src={listen} alt='listen' />
-        <h3 className='text-28xl text-black2 font-bold text-center mb-8'>BRINGING YOU THE <span className='text-28xl text-darkorange font-bold'>BEST</span> AUDIO GEAR</h3>
+        <h3 className='text-28xl text-black2 font-bold text-center mb-8 tracking-[1px]'>BRINGING YOU THE <span className='text-28xl text-darkorange font-bold'>BEST</span> AUDIO GEAR</h3>
         <p className='text-sbase text-bordergrey font-medium mb-32'>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
       </section>
     </div>
